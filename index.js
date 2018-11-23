a6964389cdd34d6747357fa69c96d77d07102a1e
Briggs Elsperger <briggse@protonmail.com>
2018-11-23 00:34:53 -0600

    adds init and mongodb connection

---
+++ index.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds115154.mlab.com:15154/configzme`)
+
+
+const app = require('./app');
+
+app.listen(7777);
