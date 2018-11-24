a8b83f26dee4edd2dc2036896e56c57959ec9d7d
Briggs Elsperger <briggse@protonmail.com>
2018-11-23 18:42:10 -0600

    adds passport

--- app/index.js
+++ app/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+// Setting up passport
+require('./handlers/passport');
+
 // Setting up routes
 const routes = require('./routes');
 app.use('/', routes);
