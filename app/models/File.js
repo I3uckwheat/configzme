e92dfe7de93df280859e11590d132a51ea52d102
Briggs Elsperger <briggse@protonmail.com>
2018-11-23 18:42:19 -0600

    adds definitions

--- app/models/File.js
+++ app/models/File.js
@@ -1,6 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const File = new Schema({});
+const File = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  contents: {
+    type: String,
+    required: true
+  },
+  extension: String
+});
 
 module.exports = mongoose.model('File', File);
--- app/models/User.js
+++ app/models/User.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
-const User = new Schema({});
+const User = new Schema({
+  // files is an array of 'files' defined by the 'file' schema
+  files: [{type: mongoose.Schema.Types.ObjectId, ref: 'File'}]
+});
 
 User.plugin(passportLocalMongoose);
 
