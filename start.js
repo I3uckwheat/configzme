67d1d8e19e271967cff0c88030ecef360048494b
Briggs Elsperger <briggse@protonmail.com>
2018-11-23 18:42:03 -0600

    refactor

--- start.js
+++ start.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
-
 require('dotenv').config();
 
 mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds115154.mlab.com:15154/configzme`, {useNewUrlParser: true})
 
 // Import all models
-require('./app/models/User');
 require('./app/models/File');
+require('./app/models/User');
 
 const app = require('./app');
 
