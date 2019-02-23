09f3a44768c9a253aec8fd909d48f3b168eeb9c6
Briggs Elsperger <briggse@protonmail.com>
2019-02-23 01:29:01 -0600

    changes messae

--- backend/app/routes.js
+++ backend/app/routes.js
@@ -7,7 +7,6 @@ const upload = multer({storage: storage})
 const userController = require('./controllers/userController');
 const authController = require('./controllers/authController');
 
-
 router.use((req, res, next) => {
   const userAgent = req.get('user-agent').split('/')[0];
   if (req.query.api === 'true' || userAgent === 'curl') {
@@ -39,8 +38,38 @@ router.post('api/:file', authController.authenticate, upload.single('file'), use
 router.patch('api/:file', authController.authenticate, upload.single('file'), userController.updateFile);
 router.delete('api/:file', authController.authenticate, userController.deleteFile);
 
-router.get('api', (req, res) => {res.send('console directions')});
+// TODO - combine these requests to remove the need for `-X POST` on register
 router.post('api', userController.register);
+router.get('api', (req, res) => {
+  const directions = (
+`------------------------------------------------------------
+Welcome to configz.me!
+------------------------------------------------------------
+
+# Registering
+curl -u -X POST <username> configz.me
+
+# Uploading Files
+curl -u <username> --form upload=@<your file> configz.me/<filename>
+
+# Getting files
+curl -u <username> configz.me/<filename>
+
+# Listing files
+curl -u <username> configz.me/files
+
+# updating files
+curl -u <username> -X PATCH -F file=@<your file> configz.me/<filename>
+
+# Deleting files
+curl -u <username> -X DELETE configz.me/<filename>
+------------------------------------------------------------
+
+`);
+
+
+  res.send(directions)
+});
 
 //  TODO - API 404
 router.use('*', (req, res) => {
