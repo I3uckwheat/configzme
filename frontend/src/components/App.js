fc720f462630c407b25725a7b49a5d59e8a6a28c
Carlos Diaz <cdiaz86@gmail.com>
2020-03-03 19:12:55 -0500

    Adds title & css/components folders.

---
+++ frontend/src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../css/App.css";
+import Title from "./Title";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <Title title="Configz.me" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
