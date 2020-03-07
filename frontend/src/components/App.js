badf5e5e33289c991405461ffb39f15d5ad0ab0a
Carlos Diaz <cdiaz86@gmail.com>
2020-03-06 21:11:50 -0500

    Renders landing based off state.

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,12 +2,28 @@ import React from "react";
 import "../css/App.css";
 import Landing from "./landing/Landing";
 
-function App() {
-  return (
-    <div className="App">
-      <Landing />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoggedIn: false,
+    username: null,
+  };
+
+  render() {
+    const userView = (isLoggedIn) => {
+      if (isLoggedIn) {
+        console.log("Logged in!");
+      } else {
+        console.log("Not Logged in!");
+        return <Landing />;
+      }
+    }
+
+    return (
+      <div className="App">
+        { userView(this.state.isLoggedIn) }
+      </div>
+    );
+  }
 }
 
 export default App;
