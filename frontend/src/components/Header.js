6001d70f7883eeb242d1eda0a50da5fdc55c2e05
Carlos Diaz <cdiaz86@gmail.com>
2020-04-10 22:19:06 -0400

    Adds add file modal.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,34 +1,51 @@
 import React from "react";
 import "../css/header.css"
+import "../css/modal-style.css"
 
 class Header extends React.Component {
   addFileForm() {
     const form = (
-      <form
-        encType="multipart/form-data"
-        method="POST"
-        autoComplete="off"
-        onSubmit={this.props.fileSubmitHandler}
-      >
-        <label htmlFor="file-name">Enter File Name:</label>
-        <input
-          type="text"
-          htmlFor="file-name"
-          name="file-name"
-          onChange={this.props.setFileName}
-          value={this.props.fileName}
-        ></input>
+      <div className="modal">
+        <div className="modal-content">
+          
+          <form
+            className={"add-file-form"}
+            encType="multipart/form-data"
+            method="POST"
+            autoComplete="off"
+            onSubmit={this.props.fileSubmitHandler}
+          >
+            
+            <label htmlFor="file-name">Enter File Name:</label>
+            <input
+              type="text"
+              htmlFor="file-name"
+              name="file-name"
+              onChange={this.props.setFileName}
+              value={this.props.fileName}
+            ></input>
 
-        <label htmlFor="file">Add a File:</label>
-        <input
-          type="file"
-          name="file"
-          id="file"
-          className="inputfile"
-          onChange={this.props.setFile}
-        />
-        <input type="submit" value="Submit"></input>
-      </form>
+            <label htmlFor="file">Add a File:</label>
+            <input
+              type="file"
+              name="file"
+              id="file"
+              className="inputfile"
+              onChange={this.props.setFile}
+            />
+            <div className="form-buttons">
+              <button 
+                onClick={() => {
+                  this.props.showAddFileForm(this.props.showAddFile);
+                }}
+                className="close-modal">
+                  Cancel
+              </button>
+              <input type="submit" value="Submit"></input>
+            </div>
+          </form>
+        </div>        
+      </div>
     );
 
     return this.props.showAddFile ? form : null;
