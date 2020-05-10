fd3bc08326912cf00ba4a466c9be86503bc768c5
Carlos Diaz <cdiaz86@gmail.com>
2020-05-10 19:14:04 -0400

    Adds input handlers and collects credentials.

--- frontend/src/components/LoginModal.js
+++ frontend/src/components/LoginModal.js
@@ -1,51 +1,95 @@
 import React from "react";
 import Modal from "./Modal";
 
-function LoginModal(props) {
-  return (
-    <Modal 
-      toggleModal={props.toggleLoginModal}
-      showModal={props.showLoginModal}
-      isModal ={true}
-      title="Login"
-    >
-      <form className="login-form">
-          <div className="input-pair">
-            <label htmlFor="username" className="input-label">Username:</label>
-            <span>
-              <span className="bracket">></span>
-              <input type="text" name="username" placeholder="Enter Username" className="input-field"></input>
-            </span>
-          </div>
-          <div className="input-pair">
-            <label htmlFor="password" className="input-label">Password:</label>
-            <span>
-              <span className="bracket">></span>
-              <input type="text" name="password" placeholder="Enter Password" className="input-field"></input>
-            </span>
-          </div>
-          <div>
-            <input
-              className="base green"
-              type="submit"
-              value="Submit"
-              onClick={event => {
-                event.preventDefault();
-                props.attemptLogin("test1", "testing123");
-              }}
-            ></input>
-            <button
-              className="close-modal base white"
-              onClick={() => {
-                props.toggleLoginModal(props.showLoginModal);
-              }}
-            >
-              Cancel
-            </button>
-          </div>
-        </form>
-    </Modal>
-  );
+class LoginModal extends React.Component{
+  state = {
+    emptyUsername: false,
+    emptyPassword: false,
+    username: '',
+    password: ''
+  }
+
+  handleUsername = (event) => {
+    this.setState({username: event.target.value});
+    if (event.target.value === "") {
+      this.setState({ emptyUsername: true });
+    } else {
+      this.setState({ emptyUsername: false });
+    }
+  }
+
+  handlePassword = (event) => {
+    this.setState({password: event.target.value});
+    if (event.target.value === "") {
+      this.setState({ emptyPassword: true });
+    } else {
+      this.setState({ emptyPassword: false });
+    }
+  }
+
+  collectCredentials = () => {
+    if (this.state.username === "") {
+      this.setState({ emptyUsername: true })
+    }
+
+    if (this.state.password === "") {
+      this.setState({ emptyPassword: true })
+    }
+
+    if (!this.state.emptyUsername && !this.state.emptyPassword) {
+      this.props.attemptLogin(this.state.username, this.state.password);
+    }
+    // this.props.attemptLogin("test1", "testing123");
+  }
+
+  render() {
+    return (
+      <Modal 
+        toggleModal={this.props.toggleLoginModal}
+        showModal={this.props.showLoginModal}
+        isModal ={true}
+        title="Login"
+        emptyUsername={this.state.emptyUsername}
+        noPassword={this.state.emptyPassword}
+      >
+        <form className="login-form">
+            <div className="input-pair">
+              <label htmlFor="username" className="input-label">Username:</label>
+              <span>
+                <span className="bracket">></span>
+                <input type="text" name="username" placeholder="Enter Username" className="input-field" onChange={this.handleUsername}></input>
+              </span>
+            </div>
+            <div className="input-pair">
+              <label htmlFor="password" className="input-label">Password:</label>
+              <span>
+                <span className="bracket">></span>
+                <input type="password" name="password" placeholder="Enter Password" className="input-field" onChange={this.handlePassword}></input>
+              </span>
+            </div>
+            <div>
+              <input
+                className="base green"
+                type="submit"
+                value="Submit"
+                onClick={event => {
+                  event.preventDefault();
+                  this.collectCredentials();
+                }}
+              ></input>
+              <button
+                className="close-modal base white"
+                onClick={() => {
+                  this.props.toggleLoginModal(this.props.showLoginModal);
+                }}
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
+      </Modal>
+    );
+  }
 }
 
 export default LoginModal;
